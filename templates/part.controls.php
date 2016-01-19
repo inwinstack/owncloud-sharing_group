cf09f42e314d63fc0ae06c3162b72566f654b778
CurlyYang <eric.y@inwinstack.com>
2016-01-19 14:55:58 +0800

    translate the omissive templates

--- templates/part.controls.php
+++ templates/part.controls.php
@@ -7,15 +7,15 @@
             </div>
 
             <ul class="sg-dropdown-menu checkuser" hidden=true>
-                <li><a href="#" id="check-all">All</a></li>
-                <li><a href="#" id="clear-all">None</a></li>
-                <li><a href="#" id="inverse">Inverse</a></li>
+                <li><a href="#" id="check-all"><?php p($l->t('All'))?></a></li>
+                <li><a href="#" id="clear-all"><?php p($l->t('None'))?></a></li>
+                <li><a href="#" id="inverse"><?php p($l->t('Inverse'))?></a></li>
             </ul>
         </div>
 
         <div class="sg-dropdown" id="sg-dropdown-group">
             <div type="button" class="sg-dropdown-toggle" id="toggle-group">
-                <span>Groups</span>
+                <span><?php p($l->t('Groups'))?></span>
                 <span class="caret" id="caret-group"></span>
             </div>
 
@@ -27,8 +27,12 @@
                 </div>
                 <div class="sg-dropdown-footer">
                     <div class="btn-group btn-group-justified">
-                        <div type="button" class="btn-flat" id="cancel">CANCEL</div>
-                        <div type="button" class="btn-flat" id="multi-group-select">APPLY</div>
+                        <div type="button" class="btn-flat" id="cancel">
+                            <?php p($l->t('CANCEL'))?>
+                        </div>
+                        <div type="button" class="btn-flat" id="multi-group-select">
+                            <?php p($l->t('APPLY'))?>
+                        </div>
                     </div>
                 </div>
             </div>
