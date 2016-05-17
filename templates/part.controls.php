8f2eee3be507523536eb5718ae038b679405e355
CurlyYang <eric.y@inwinstack.com>
2016-05-17 15:11:21 +0800

    Add search input and cancel button

--- templates/part.controls.php
+++ templates/part.controls.php
@@ -36,52 +36,56 @@
                 </div>
             </div>
         </div>
-
-        <div class="user-listed">
-            <?php if(\OC_Config::getValue('sharing_group_mode') == 'Friend_mode'){ ?>
-                <button type="button" id="sg-addfriend">
-                    <?php p($l->t('Add friends'))?>
-                </button>
-                <button type="button" id="sg-deletefriend" disabled="disabled">
-                    <?php p($l->t('Delete friends'))?>
-                </button>
-                <button class="export" id="export"><?php p($l->t('export'))?></button>
-                <button class="import" id="import"><?php p($l->t('import'))?></button>
-
-                <div id="sg-dialog" title="<?php p($l->t('Add friends')) ?>">
-                    <form class="sg-searchbox" action="" method="GET">
-                        <input id="sg-friend-searchbox" placeholder="test@email.com" type="search" tabindex="5">
-                        <input type="submit" class="sg-searchbox-submit" value="<?php p($l->t('Search')) ?>">
-                    </form>
-                    <span class='sg-friend-name'></span>
-                </div>
-            <?php } ?>
+    </div>    
             
+    <div class="user-listed">
+        <?php if(\OC_Config::getValue('sharing_group_mode') == 'Friend_mode'){ ?>
+            <button type="button" id="sg-addfriend">
+                <?php p($l->t('Add friends'))?>
+            </button>
+            <button type="button" id="sg-deletefriend" disabled="disabled">
+                <?php p($l->t('Delete friends'))?>
+            </button>
+            <button class="export" id="export"><?php p($l->t('export'))?></button>
+            <button class="import" id="import"><?php p($l->t('import'))?></button>
 
-            <div class="sg-dropdown hidden" id="sg-dropdown-load">
-                <div type="button" class="sg-dropdown-toggle" id="toggle-load">
-                    <span><?php p($l->t('Load'))?></span>
-                    <span class="caret" id="caret-load"></span>
-                </div>
-                <ul class="sg-dropdown-menu load" >
-                    <li class="load-part-users"><a href="#"><?php p($l->t('Load next 100 users'))?></a></li>
-                    <li class="load-all-users"><a href="#"><?php p($l->t('Load all users'))?></a></li>
-                </ul>
+            <div id="sg-dialog" title="<?php p($l->t('Add friends')) ?>">
+                <form class="sg-searchbox" action="" method="GET">
+                    <input id="sg-addfriend-searchbox" placeholder="test@email.com" type="search" tabindex="5">
+                    <input type="submit" class="sg-searchbox-submit" value="<?php p($l->t('Search')) ?>">
+                </form>
+                <span class='sg-friend-name'></span>
             </div>
+        <?php } ?>
+        
 
-
-            <button type="button" class="load-part-users hidden" disabled="disabled">
-                <?php p($l->t('Load next 100 users'))?>
-            </button>
-
-            <button type="button" class="load-all-users hidden" disabled="disabled">
-                <?php p($l->t('Load all users'))?>
-            </button>
-
-            <span> <?php  p($l->t('Has been shown'))?></span>
-            <span class="users-offset">?</span>
-            <span> / </span>
-            <span class="all-users-count">?</span>
+        <div class="sg-dropdown hidden" id="sg-dropdown-load">
+            <div type="button" class="sg-dropdown-toggle" id="toggle-load">
+                <span><?php p($l->t('Load'))?></span>
+                <span class="caret" id="caret-load"></span>
+            </div>
+            <ul class="sg-dropdown-menu load" >
+                <li class="load-part-users"><a href="#"><?php p($l->t('Load next 100 users'))?></a></li>
+                <li class="load-all-users"><a href="#"><?php p($l->t('Load all users'))?></a></li>
+            </ul>
         </div>
+        
+
+        <button type="button" class="load-part-users hidden" disabled="disabled">
+            <?php p($l->t('Load next 100 users'))?>
+        </button>
+
+        <button type="button" class="load-all-users hidden" disabled="disabled">
+            <?php p($l->t('Load all users'))?>
+        </button>
+
+        <span> <?php  p($l->t('Has been shown'))?></span>
+        <span class="users-offset">?</span>
+        <span> / </span>
+        <span class="all-users-count">?</span>
     </div>
+    <input id="sg-searchfriend-searchbox" type="input" tabindex="5">
+    <button type="button" id="sg-searchfriend-cancel" style="display:none">
+        <?php p($l->t('Cancel'))?>
+    </button>
 </div>
